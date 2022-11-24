13809cb5f05d4e35f0002db0d720ea09f7f70e04
Yogeshwar <yogihodor09@outlook.com>
2022-11-24 12:37:41 +0530

    drawing app initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/yogesh/kidsdrawingapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yogesh.kidsdrawingapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yogesh.kidsdrawingapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
